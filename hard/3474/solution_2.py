1e2a3df4429f132705d031542007b24294303e63
Chetanbhai Ranpariya <chetanbhai.ranpariya@autodesk.com>
2026-04-01 21:58:49 +0800

    Added solutions for problem 3474.

---
+++ hard/3474/solution_2.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/lexicographically-smallest-generated-string
+
+def generateString(str1: str, str2:str) -> str:
+    n, m = len(str1), len(str2)
+    s = ["a"] * (n + m - 1)
+    fixed = [False] * (n + m - 1)
+
+    for i, ch in enumerate(str1):
+        if ch == "T":
+            for j, c in enumerate(str2, i):
+                if fixed[j] and s[j] != c:
+                    return ""
+                s[j], fixed[j] = c, True
+
+    for i, ch in enumerate(str1):
+        if ch == "F":
+            if any(str2[j - i] != s[j] for j in range(i, i + m)):
+                continue
+
+            for j in range(i + m - 1, i - 1, -1):
+                if not fixed[j]:
+                    s[j] = "b"
+                    break
+            else:
+                return ""
+
+    return "".join(s)
+
+
+
+str = 'TFTF'
+str2 = 'ab'
+
+print(generateString(str, str2))
